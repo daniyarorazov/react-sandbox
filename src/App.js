986133fe18f92bf6d2ac3072b3142b12555ee7c7
Daniyar Orazov <orazov.video@gmail.com>
2022-10-18 20:32:49 +0200

    Added CRUD System without DB, with local array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ItemsListComponent from "./components/ItemsList/ItemsListComponent";
+import SearchElementsFormComponent from "./components/SearchElements/SearchElementsFormComponent";
+import {datas} from "./data";
+import {Link} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const search_query = new URLSearchParams(window.location.search);
+    let q = search_query.get('q');
+
+    console.log(datas)
+
+    return (
+        <div className="App">
+            <Link to="/add-project">Add projects</Link>
+            <SearchElementsFormComponent linkListId={"cardsList"} />
+
+            <ItemsListComponent
+                className="list"
+                id="cardsList"
+                classNameElements="list-item"
+                listItemsData={datas}
+                searchQuery={q}
+            />
+            {"Data is: " + q}
+        </div>
+    );
 }
 
 export default App;
